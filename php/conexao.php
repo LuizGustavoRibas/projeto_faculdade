ff3259da1637ffc477e5ca54bf3ecd94c2dd156d
Luiz Ribas <lgribas03@otmail.com>
2022-06-01 20:09:16 -0300

    first commit

---
+++ php/conexao.php
@@ -0,0 +1,13 @@
+<?php
+  
+  $dbHost = 'Localhost';
+  $dbUsername = 'root';
+  $dbPassword = '';
+  $dbName = 'formulario_pagina';
+
+  //aqui conecta
+  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+  
+
+  
+?>
